c9a42057a9e6600eca918fccb84d48b557df1801
Ned Palacios <7358345+nedpals@users.noreply.github.com>
2021-10-04 14:58:25 +0800

    update: session and attendence class; add setArrayIndex method (#14)
    * add: JSON database system
    
    * update: gitignore contents
    
    * add: attendance.java and sesson.java
    
    * add: admin entity example
    
    * add: setArrayIndex method
    
    * add: Date2 class for toJSON and fromJSON
    
    * update: Session and Attendance code

--- src/schoolmanagementsystem/Admin.java
+++ src/schoolmanagementsystem/Admin.java
@@ -15,20 +15,13 @@ import org.json.simple.JSONArray;
 public class Admin {
     public int id;
     public String username;
-    private int arrayIndex = -1;
     private String password;
+    private int arrayIndex = -1;
     
     Admin(int id, String username, String password) {
         this.id = id;
         this.username = username;
         this.password = password;
-    }
-    
-    Admin(int id, String username, String password, int arrayIndex) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.arrayIndex = arrayIndex;
     }
     
     public static Admin fromJSON(JSONObject obj) {
@@ -39,7 +32,9 @@ public class Admin {
         int adminID = (int) (long) obj.get("id");
         String adminUsername = (String) obj.get("username");
         String adminPassword = (String) obj.get("password");
-        return new Admin(adminID, adminUsername, adminPassword, arrayIndex);
+        Admin adm = new Admin(adminID, adminUsername, adminPassword);
+        adm.setArrayIndex(arrayIndex);
+        return adm;
     }
     
     public JSONObject toJSON() {
@@ -66,6 +61,10 @@ public class Admin {
             System.out.println(ex);
             return null;
         }
+    }
+    
+    public void setArrayIndex(int newIndex) {
+        this.arrayIndex = newIndex;
     }
     
     public void save() throws Exception {
---
+++ src/schoolmanagementsystem/Date2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schoolmanagementsystem;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author nedpals
+ */
+public class Date2 {
+    private Date rawDate;
+    private static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm a z");
+    
+    Date2() {
+        this.rawDate = new Date();
+    }
+    
+    Date2(Date rawDate) {
+        this.rawDate = rawDate;
+    }
+    
+    public static Date2 fromJSON(String rawStrDate) throws Exception {
+        Date gotDate = Date2.formatter.parse(rawStrDate);
+        return new Date2(gotDate);
+    }
+    
+    public String toJSON() {
+        return Date2.formatter.format(rawDate);
+    }
+}
