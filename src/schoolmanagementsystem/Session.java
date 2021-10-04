87b21b98c19405e9818e691e4d347a0f27b182a5
Ned Palacios <7358345+nedpals@users.noreply.github.com>
2021-10-04 13:23:33 +0800

    add: session and attendance class; add admin database example (#13)
    * add: JSON database system
    
    * update: gitignore contents
    
    * add: attendance.java and sesson.java
    
    * add: admin entity example

---
+++ src/schoolmanagementsystem/Session.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schoolmanagementsystem;
+
+import java.util.Date;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author nedpals
+ */
+//public class Session {
+//    public int id;
+//    public Date heldAt;
+//    public int subjectId;
+//    public String title;
+//    public String description;
+//    public int[] attendanceIds;
+//    
+//    public Subject getSubject() {
+//        
+//    }
+//}
