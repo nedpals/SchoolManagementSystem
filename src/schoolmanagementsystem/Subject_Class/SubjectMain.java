8d38242ca7694c291b45d7ebab17bb49ba2e4f6b
jparcasio <90225992+jparcasio@users.noreply.github.com>
2021-10-05 15:05:50 +0800

    ADD Subject Class GUI

---
+++ src/schoolmanagementsystem/Subject_Class/SubjectMain.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schoolmanagementsystem.Subject_Class;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author Parcasio
+ */
+public class SubjectMain {
+public static String subjectPath = "src/schoolmanagementsystem.Subject_Class.Database/Subject.json";
+public static String sessionPath = "src/schoolmanagementsystem.Subject_Class.Database/Session.json";
+public static String NotePath = "src/schoolmanagementsystem.Subject_Class.Database/Note.json";
+    public static void main(String[] args) {
+     
+    } 
+}
